5299666e13d7142ce25395b60119fd1db9f516f2
norbigith <dulnorbert@gmail.com>
2021-11-18 19:07:47 +0100

    Works fine

---
+++ FormData.js
@@ -0,0 +1,374 @@
+let text=`
+Log
+()()((()
+Test Passed: Value == false
+Log
+()
+Test Passed: Value == true
+Log
+()()
+Test Passed: Value == true
+Log
+(())
+Test Passed: Value == true
+Log
+)
+Test Passed: Value == false
+Test Passed: Value == true
+Log
+())
+Test Passed: Value == false
+Log
+((((()))))
+Test Passed: Value == true
+Log
+()))
+Test Passed: Value == false
+Log
+()()()())
+Test Passed: Value == false
+Log
+(()()()())(())
+Test Passed: Value == true
+Log
+((((((((
+Test Passed: Value == false
+Log
+(())((()((()))))
+Test Passed: Value == true
+Log
+())(
+Test Passed: Value == false
+Log
+)()()()(
+Test Passed: Value == false
+Log
+(()()))(
+Test Passed: Value == false
+Log
+)()(
+Test Passed: Value == false
+Log
+())(()
+Test Passed: Value == false
+Log
+())(()
+Test Passed: Value == false
+Random tests
+Log
+()()()(())
+Test Passed: Value == true
+Log
+()()()
+Test Passed: Value == true
+Log
+()()()()()()()()()()()()()
+Test Passed: Value == true
+Log
+()()()()()()()()()()()()()()()()()()((
+Test Passed: Value == false
+Log
+(())()(())((()
+Test Passed: Value == false
+Log
+()()()()()()()()(())()()()()()()()()(())
+Test Passed: Value == true
+Log
+()()(())
+Test Passed: Value == true
+Log
+()((()))
+Test Passed: Value == true
+Log
+()()()()(()()())()()()()()
+Test Passed: Value == true
+Log
+(())()()()()()()
+Test Passed: Value == true
+Log
+(())()()()()(())()()()()()()(()()())()((
+Test Passed: Value == false
+Log
+()()()(())()()
+Test Passed: Value == true
+Log
+()()(())(())()()()(()(
+Test Passed: Value == false
+Log
+()()()()(())()()(())()
+Test Passed: Value == true
+Log
+()()()()()()()()()(())()()()()()
+Test Passed: Value == true
+Log
+()((()))()()(())()()()()(())()()(())
+Test Passed: Value == true
+Log
+()()()()()()()()
+Test Passed: Value == true
+Log
+()()()()()()()()
+Test Passed: Value == true
+Log
+()()(())
+Test Passed: Value == true
+Log
+()()()()
+Test Passed: Value == true
+Log
+((()))()()()()()()()()(()())((
+Test Passed: Value == false
+Log
+()()()()()()()()()()(()())(())()
+Test Passed: Value == true
+Log
+((())())()()()()()()(()())()()()()()
+Test Passed: Value == true
+Log
+()()()()()()
+Test Passed: Value == true
+Log
+()((
+Test Passed: Value == false
+Log
+(())
+Test Passed: Value == true
+Log
+(())()
+Test Passed: Value == true
+Log
+()()()()(())()()(())()(())()()()
+Test Passed: Value == true
+Log
+()()(())(())()()()()()
+Test Passed: Value == true
+Log
+()()()
+Test Passed: Value == true
+Log
+()()()()(())()()()((
+Test Passed: Value == false
+Log
+()(())()()()()(())()()()()()()(())()()
+Test Passed: Value == true
+Log
+(())((()))()()()()()()()()((())())(())
+Test Passed: Value == true
+Log
+()(())()()()()()()()
+Test Passed: Value == true
+Log
+()()()
+Test Passed: Value == true
+Log
+()(())(())()()()()()()()()()()
+Test Passed: Value == true
+Log
+()()(()())()()()()()()()
+Test Passed: Value == true
+Log
+()()()(()())(())()()()()(())()()()()
+Test Passed: Value == true
+Log
+()()()()()(())()()()()()()()((
+Test Passed: Value == false
+Log
+()()()()()()()()()()()()()()()()
+Test Passed: Value == true
+Log
+()(())
+Test Passed: Value == true
+Log
+()()()()()()()()()()()()()()()()()
+Test Passed: Value == true
+Log
+()()()(())()()()()()(())
+Test Passed: Value == true
+Log
+()(())()()()(())()
+Test Passed: Value == true
+Log
+()(()(())())()(())()()()()()()()()()
+Test Passed: Value == true
+Log
+()()()()()()
+Test Passed: Value == true
+Log
+()()()(())()()()((
+Test Passed: Value == false
+Log
+()()(())()()()()()()()()()()()()()((
+Test Passed: Value == false
+Log
+()()()()()()()()()()()()()()()()()()
+Test Passed: Value == true
+Log
+()
+Test Passed: Value == true
+Log
+()()()()()()(())()()()()()()
+Test Passed: Value == true
+Log
+()()()()(())()()()()()()(())()()()()()
+Test Passed: Value == true
+Log
+(())()()(())()()()()()()()()()()()
+Test Passed: Value == true
+Log
+()()()()()()
+Test Passed: Value == true
+Log
+()()()()()()()(())()()()()()()
+Test Passed: Value == true
+Log
+()()(())()()()()(())()
+Test Passed: Value == true
+Log
+()()()()()()()()(())(()())()()()()()()
+Test Passed: Value == true
+Log
+()()()()(((())()))
+Test Passed: Value == true
+Log
+()()()()()()()
+Test Passed: Value == true
+Log
+()()
+Test Passed: Value == true
+Log
+(()())()()()()(())()()()()()()()()()
+Test Passed: Value == true
+Log
+()()()()()()()()()(())
+Test Passed: Value == true
+Log
+()()()()
+Test Passed: Value == true
+Log
+()()(())()(())()()()()
+Test Passed: Value == true
+Log
+()()()(()())()()()()()
+Test Passed: Value == true
+Log
+()()()()()
+Test Passed: Value == true
+Log
+()()()()()()
+Test Passed: Value == true
+Log
+(()())(())()()()()()()()()()()()(())()
+Test Passed: Value == true
+Log
+()()()
+Test Passed: Value == true
+Log
+()(())((
+Test Passed: Value == false
+Log
+()()(())()()()()()()()()()(())()(()())((
+Test Passed: Value == false
+Log
+(()())()
+Test Passed: Value == true
+Log
+()(())()()()
+Test Passed: Value == true
+Log
+()()()()()()()()()()()()()()()
+Test Passed: Value == true
+Log
+()(())()()()()
+Test Passed: Value == true
+Log
+()()(())(())
+Test Passed: Value == true
+Log
+()()()()()()()()()()
+Test Passed: Value == true
+Log
+(())()
+Test Passed: Value == true
+Log
+()
+Test Passed: Value == true
+Log
+(())()()()(())()()()()(())
+Test Passed: Value == true
+Log
+()((()
+Test Passed: Value == false
+Log
+()()(())()()()()()()()()()()()()
+Test Passed: Value == true
+Log
+()()(())()()()()(())()()()()((
+Test Passed: Value == false
+Log
+(()())()()
+Test Passed: Value == true
+Log
+(())()()()()()()()(())(())()()()()(())()
+Test Passed: Value == true
+Log
+()()()()()()()(())()()()()(())(())()
+Test Passed: Value == true
+Log
+()()()()()(())
+Test Passed: Value == true
+Log
+()()
+Test Passed: Value == true
+Log
+(())(())()()()((
+Test Passed: Value == false
+Log
+(()())()(())()(())()()()()
+Test Passed: Value == true
+Log
+()()()()()()()()()(())()(())()()()((
+Test Passed: Value == false
+Log
+()()()()()()()()()()(())()()()()
+Test Passed: Value == true
+Log
+()()
+Test Passed: Value == true
+Log
+()()(())()()()()()
+Test Passed: Value == true
+Log
+()()()()(())
+Test Passed: Value == true
+Log
+()
+Test Passed: Value == true
+Log
+()()()()()()()()()()()()()
+Test Passed: Value == true
+Log
+()(())()()()
+Test Passed: Value == true
+Log
+(())()()()()()()
+Test Passed: Value == true
+Log
+()()()()()()(()()())()()()()
+Test Passed: Value == true`
+
+
+function makeIToARightFormat(changeit){
+    
+    let howManyTimesAreLogInText=changeit.match(/Log/g).length;
+    let result;
+    for(let i=0;i<=howManyTimesAreLogInText;i++){
+
+    changeit = changeit.replace(/log/i,`${i}.`);
+    changeit = changeit.replace(/Test Passed: Value == true/i,`this is true \n`);
+    changeit = changeit.replace(/Test Passed: Value == false/i,`this is false \n` );
+        
+    }
+    console.log(changeit);
+}
+
+makeIToARightFormat(text);
